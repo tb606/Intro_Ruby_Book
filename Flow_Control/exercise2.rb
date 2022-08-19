ba2d51126d58b5691732c1ff675a7580a33b2010
Tristan Brown <tristangerardbrown@gmail.com>
2022-08-19 00:17:08 -0700

    Flow_Control

---
+++ Flow_Control/exercise2.rb
@@ -0,0 +1,13 @@
+def greeting(string)
+	if string.length > 10 
+	 string.upcase
+	else
+	 string 
+end 
+end 
+
+puts greeting("Hello World")
+puts greeting("hi")
+
+
+
---
+++ Flow_Control/exercise3.rb
@@ -0,0 +1,17 @@
+def numbering(i)
+	if i <= 50
+		puts "#{i} is between 0 and 50"
+	elsif  i <= 100
+		puts  "#{i} is between 51 and 100"
+	else 
+		puts  "#{i} is above 100"
+	end 
+ end  
+ 
+
+puts numbering(101)
+puts numbering(53)
+puts numbering(13)
+
+
+
